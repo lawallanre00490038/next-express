98160f0b2fc2c0d1ca80be79b08d2e9cff4c6710
Olanrewaju <lawallanre.abbahouse@gmail.com>
2024-08-16 09:49:24 +0100

    tese

--- frontend/app/post/page.tsx
+++ frontend/app/post/page.tsx
@@ -1,12 +1,25 @@
 'use client'
- 
+import React from 'react';
+import { useQuery } from 'react-query';
  
 export default function SearchBar() {
+
+  const fetchData = async () => {
+    const response = await fetch('http://localhost:5000/api/exp');
+    return response.json();
+  };
+
+  const { data, isLoading } = useQuery('data', fetchData);
+
+  if (!isLoading) {
+    return <div>Loading...</div>;
+  }
+
  
  
   return (
     <div>
-      I am a search bar
+      I am a search bar, {data}
     </div>
   )
 }
--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -2,7 +2,15 @@
 const nextConfig = {
   experimental: {
     missingSuspenseWithCSRBailout: false,
-  }
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/exp',
+        destination: 'http://localhost:5000/api/exp',
+      },
+    ]
+  },
 };
 
 export default nextConfig;
